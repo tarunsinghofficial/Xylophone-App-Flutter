100d9736d49827436683ad2b1ad8aca1fe0504ca
tarunsinghofficial <tarunsinghwap7@gmail.com>
2021-01-12 13:31:21 +0530

    Xylophone App

---
+++ android/app/src/main/kotlin/com/example/Xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  void playSound(int sN) {
+    AudioCache player = AudioCache();
+    player.play('note$sN.wav');
+  }
+
+  Expanded buildKey({Color color, int sN}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(sN);
+        },
+        child: null,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                buildKey(color: Colors.red, sN: 1),
+                buildKey(color: Colors.orange, sN: 2),
+                buildKey(color: Colors.yellow, sN: 3),
+                buildKey(color: Colors.green, sN: 4),
+                buildKey(color: Colors.teal, sN: 5),
+                buildKey(color: Colors.blue, sN: 6),
+                buildKey(color: Colors.purple, sN: 7),
+              ]
+              // Expanded(
+              //   child: FlatButton(
+              //     color: Colors.yellow,
+              //     onPressed: () {
+              //       playSound(2);
+              //     },
+              //     child: null,
+              //   ),
+              // ),
+              // Expanded(
+              //   child: FlatButton(
+              //     color: Colors.green,
+              //     onPressed: () {
+              //       playSound(3);
+              //     },
+              //     child: null,
+              //   ),
+              // ),
+              // Expanded(
+              //   child: FlatButton(
+              //     color: Colors.blue,
+              //     onPressed: () {
+              //       playSound(4);
+              //     },
+              //     child: null,
+              //   ),
+              // ),
+              // Expanded(
+              //   child: FlatButton(
+              //     color: Colors.orange,
+              //     onPressed: () {
+              //       playSound(5);
+              //     },
+              //     child: null,
+              //   ),
+              // ),
+              // Expanded(
+              //   child: FlatButton(
+              //     color: Colors.teal,
+              //     onPressed: () {
+              //       playSound(6);
+              //     },
+              //     child: null,
+              //   ),
+              // ),
+              // Expanded(
+              //   child: FlatButton(
+              //     color: Colors.red,
+              //     onPressed: () {
+              //       playSound(7);
+              //     },
+              //     child: null,
+              //   ),
+              // ),
+
+              ),
+        ),
+      ),
+    );
+  }
+}
